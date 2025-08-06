620ff9bdfd58141192cf13c4305e58e1c527cf2b
CharChampion <charvikpenjarla@gmail.com>
2025-08-06 17:52:55 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+m = 0
+b = 0
+a = str(input("Enter a number. The computer will tell how many digits are there."))
+while m == 0:
+    if "1" in a:
+        b = b + 1
+    for "2" in a:
+        b = b + 1
+    for "3" in a:
+        b = b + 1
+    for "4" in a:
+        b = b + 1
+    for "5" in a:
+        b = b + 1
+    for "6" in a:
+        b = b + 1
+    for "7" in a:
+        b = b + 1
+    for "8" in a:
+        b = b + 1
+    for "9" in a:
+        b = b + 1
+    for "0" in a:
+        b = b + 1
+print(b)
