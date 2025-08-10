351190ba613a5ccdc70c5f9f3a7b027ba58d5dc0
CharChampion <charvikpenjarla@gmail.com>
2025-08-10 09:39:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
-m = 0
 b = 0
-a = str(input("Enter a number. The computer will tell how many digits are there."))
-while m == 0:
-    if "1" in a:
-        b = b + 1
-    for "2" in a:
-        b = b + 1
-    for "3" in a:
-        b = b + 1
-    for "4" in a:
-        b = b + 1
-    for "5" in a:
-        b = b + 1
-    for "6" in a:
-        b = b + 1
-    for "7" in a:
-        b = b + 1
-    for "8" in a:
-        b = b + 1
-    for "9" in a:
-        b = b + 1
-    for "0" in a:
-        b = b + 1
+a = 0
+i = 0
+a = int(input("Enter a number. The computer will tell how many digits are there."))
+print(a)
+while i == 0:
+    if 1 in i:
+        b == b + 1
+    if 2 in i:
+        b == b + 1
+    if 3 in i:
+        b == b + 1
+    if 4 in i:
+        b == b + 1
+    if 5 in i:
+        b == b + 1
+    if 6 in i:
+        b == b + 1
+    if 7 in i:
+        b == b + 1
+    if 8 in i:
+        b == b + 1
+    if 9 in i:
+        b == b + 1
+    if 0 in i:
+        b == b + 1
 print(b)
